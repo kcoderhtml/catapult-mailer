7ec76b02a720f90a70f88072387d2cd25baae22b
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-03 15:42:05 -0400

    feat: add compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import netlify from "@astrojs/netlify";
+import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
-  integrations: [tailwind()]
+	output: "static",
+	adapter: netlify(),
+	integrations: [tailwind(), playformCompress()],
 });
