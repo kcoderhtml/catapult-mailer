4842b3dd98a741655fd8afb1842b53306921c2d8
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-06 12:14:08 -0400

    feat: add arcjet email validation

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,12 +1,25 @@
 import type { APIRoute } from 'astro';
 import { db, Subscribers } from 'astro:db';
 
+import arcjet, { validateEmail } from "@arcjet/node";
+import type { ArcjetNodeRequest } from "@arcjet/node";
+
 function generateSecret() {
     const secret = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     return secret;
 }
 
 export const POST: APIRoute = async ({ params, request }) => {
+    const aj = arcjet({
+        key: process.env.ARCJET_KEY!,
+        rules: [
+            validateEmail({
+                mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+                block: ["INVALID", "NO_MX_RECORDS"], // block invalid email addresses with no MX records
+            }),
+        ],
+    });
+
     try {
         if (request.headers.get("Content-Type") === "application/json") {
             const json = await request.json()
@@ -17,20 +30,41 @@ export const POST: APIRoute = async ({ params, request }) => {
                 throw new Error("Name is required");
             }
 
-            // Save the email to the database
-            await db.insert(Subscribers).values({
-                name: json.name,
+            const arcjetRequest: ArcjetNodeRequest = {
+                method: request.method,
+                url: request.url,
+                headers: Object.fromEntries(request.headers),
+            };
+
+            const decision = await aj.protect(arcjetRequest, {
                 email: json.email,
-                subscribedAt: new Date(),
-                secret: generateSecret()
             });
 
-            return new Response(JSON.stringify({ ok: true, message: "subscribed", email: json.email, name: json.name }), {
-                status: 200,
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+            console.log("Arcjet decision", decision);
+
+            if (decision.isDenied()) {
+                return new Response(JSON.stringify({ ok: false, reason: decision.reason }), {
+                    status: 400,
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+            } else {
+                // Save the email to the database
+                await db.insert(Subscribers).values({
+                    name: json.name,
+                    email: json.email,
+                    subscribedAt: new Date(),
+                    secret: generateSecret()
+                });
+
+                return new Response(JSON.stringify({ ok: true, message: "subscribed", email: json.email, name: json.name }), {
+                    status: 200,
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+            }
         } else {
             throw new Error("Invalid Content-Type");
         }
