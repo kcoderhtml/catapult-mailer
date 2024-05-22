63fa44abb983878215f53e781ff120a1c6c980d7
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-22 12:42:44 -0400

    feat: add webvitals

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
-
 import db from "@astrojs/db";
+
+import webVitals from "@astrojs/web-vitals";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: netlify(),
-	integrations: [tailwind(), playformCompress(), db()],
+  output: "server",
+  adapter: netlify(),
+  integrations: [tailwind(), playformCompress(), db(), webVitals()]
 });
